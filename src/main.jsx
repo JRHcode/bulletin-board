b29ef777e2931cd75c03f83108982af9d80d0f6e
Happy Lobster <179049569+JRHcode@users.noreply.github.com>
2025-04-16 23:20:02 -0700

    React Redux Thunk
    Update to include Thunk functionality

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React, { StrictMode } from 'react'
-import ReactDOM from 'react-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
+import { fetchUsers } from './features/users/usersSlice.jsx'
+
+store.dispatch(fetchUsers());
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
